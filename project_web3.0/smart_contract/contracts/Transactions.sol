5dbe25636f3c85ad86286c1b57d2e9c17ae09b6b
ADEY RATNA SHAH <115475555+AdeyRatnaShah@users.noreply.github.com>
2023-10-28 17:54:53 +0530

    Update Transactions.sol

--- project_web3.0/smart_contract/contracts/Transactions.sol
+++ project_web3.0/smart_contract/contracts/Transactions.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-
 import "hardhat/console.sol";
+
 
 contract Transactions {
     uint256 transactionCount;
-
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-  
     struct TransferStruct {
         address sender;
         address receiver;
@@ -17,6 +15,7 @@ contract Transactions {
         uint256 timestamp;
         string keyword;
     }
+
 
     TransferStruct[] transactions;
 
@@ -27,9 +26,11 @@ contract Transactions {
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
+
     function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
     }
+
 
     function getTransactionCount() public view returns (uint256) {
         return transactionCount;
