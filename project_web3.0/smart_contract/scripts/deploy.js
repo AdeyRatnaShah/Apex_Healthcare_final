1ade50a398c31020630195e66277290de082f525
ADEY RATNA SHAH <115475555+AdeyRatnaShah@users.noreply.github.com>
2023-10-28 17:53:53 +0530

    Update deploy.js

--- project_web3.0/smart_contract/scripts/deploy.js
+++ project_web3.0/smart_contract/scripts/deploy.js
@@ -1,11 +1,10 @@
 const main = async () => {
   const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
   const transactionsContract = await transactionsFactory.deploy();
-
   await transactionsContract.deployed();
-
   console.log("Transactions address: ", transactionsContract.address);
 };
+
 
 const runMain = async () => {
   try {
